3680d5c51f893ad12609f10fac10a93a756bf1c9
Jesus Cidoncha <jesus.cidoncha.berlanga@gmail.com>
2023-12-14 15:53:35 +0100

    fully working lab

--- src/App.jsx
+++ src/App.jsx
@@ -15,40 +15,105 @@ function App() {
   const [program, setProgram] = useState("");
   const [graduationYear, setGraduationYear] = useState(0);
   const [graduated, setGraduated] = useState(false);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newStudent = {
+      fullName: fullname,
+      image: image,
+      phone: phone,
+      email: email,
+      program: program,
+      graduationYear: graduationYear,
+      graduated: graduated,
+    };
+
+    setStudents([...students, newStudent]);
+
+    setFullname("");
+    setImage("");
+    setPhone("");
+    setEmail("");
+    setProgram("");
+    setGraduationYear(0);
+    setGraduated(false);
+  };
 
   return (
     <div className="App pt-20">
       <Navbar />
 
       {/* FORM */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <span>Add a Student</span>
         <div>
           <label>
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input
+              name="fullName"
+              type="text"
+              placeholder="Full Name"
+              value={fullname}
+              onChange={(event) => {
+                console.log(event.target.value);
+                setFullname(event.target.value);
+              }}
+            />
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input
+              name="image"
+              type="url"
+              placeholder="Profile Image"
+              value={image}
+              onChange={(event) => {
+                console.log(event.target.value);
+                setImage(event.target.value);
+              }}
+            />
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input
+              name="phone"
+              type="tel"
+              placeholder="Phone"
+              value={phone}
+              onChange={(event) => {
+                console.log(event.target.value);
+                setPhone(event.target.value);
+              }}
+            />
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input
+              name="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(event) => {
+                console.log(event.target.value);
+                setEmail(event.target.value);
+              }}
+            />
           </label>
         </div>
 
         <div>
           <label>
             Program
-            <select name="program">
+            <select
+              name="program"
+              value={program}
+              onChange={(event) => {
+                console.log(event.target.value);
+                setProgram(event.target.value);
+              }}
+            >
               <option value="">-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
@@ -66,12 +131,23 @@ function App() {
               maxLength={4}
               min={2023}
               max={2030}
+              value={graduationYear}
+              onChange={(event) => {
+                console.log(event.target.value);
+                setGraduationYear(event.target.value);
+              }}
             />
           </label>
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input
+              name="graduated"
+              type="checkbox"
+              value={graduated}
+              checked={graduated}
+              onChange={() => setGraduated(!graduated)}
+            />
           </label>
 
           <button type="submit">Add Student</button>
